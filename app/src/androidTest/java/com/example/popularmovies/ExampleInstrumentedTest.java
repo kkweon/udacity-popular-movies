2f0a4954712809cc7b185f43cce7ac17505b0836
Mo Kweon <kkweon@google.com>
2020-03-28 20:27:55 -0700

    Add Picasso with INTERNET permission

--- app/src/androidTest/java/com/example/popularmovies/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/popularmovies/ExampleInstrumentedTest.java
@@ -1,14 +1,12 @@
 package com.example.popularmovies;
 
+import static org.junit.Assert.*;
+
 import android.content.Context;
-
-import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-
+import androidx.test.platform.app.InstrumentationRegistry;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,8 +18,7 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation()
-                                                    .getTargetContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.example.popularmovies", appContext.getPackageName());
     }
--- app/src/main/java/com/example/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/popularmovies/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.popularmovies;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.ImageView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.image_view_movie_posters);
+
+        Picasso.get().setLoggingEnabled(true);
+        Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(imageView);
     }
 }
